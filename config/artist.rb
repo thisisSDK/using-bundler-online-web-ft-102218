ef2e3c5a09d516590e21d0ed90ab0014fa737cc6
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2018-11-14 10:09:38 -0500

    Done.

---
+++ config/artist.rb
@@ -0,0 +1,36 @@
+require_relative '../lib/concerns/memorable'
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  @@artists = []
+
+  def initialize
+    @@artists << self
+    @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
